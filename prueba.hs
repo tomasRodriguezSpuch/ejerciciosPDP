313e1f60a4b038e85a94bb78f7d7f41aa49a7adc
tomasRodriguezSpuch <trodriguezspuch@frba.utn.edu.ar>
2024-04-06 19:30:52 -0300

    Pinos terminado

--- prueba.hs
+++ prueba.hs
@@ -1,2 +1,19 @@
-suma :: Int -> Int -> Int 
-suma x y = x + y
+--1
+pesoPino :: Int -> Int
+pesoPino altura = primerosMetros altura * 300 + metrosRestantes altura * 200
+
+primerosMetros :: Int -> Int
+primerosMetros = min 3
+
+metrosRestantes :: Int -> Int
+metrosRestantes altura = max 0 (altura - 3)
+
+--2
+
+esPesoUtil :: Int -> Bool
+esPesoUtil peso = peso > 400 && peso < 1000
+
+--3
+
+sirvePino :: Int -> Bool
+sirvePino = esPesoUtil . pesoPino
