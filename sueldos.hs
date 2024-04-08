389b4e7e463a0090c38bc8ad52a84d2cbfb5c4a9
tomasRodriguezSpuch <trodriguezspuch@frba.utn.edu.ar>
2024-04-08 17:18:07 -0300

    Primer parte del trabajo SUELDOS

---
+++ sueldos.hs
@@ -0,0 +1,22 @@
+
+sueldoPorCargo :: String -> Float
+sueldoPorCargo "titular" = 149000
+sueldoPorCargo "adjunto" = 116000
+sueldoPorCargo "ayudante" = 66000
+
+bonusPorAntiguedad :: Int -> Float
+bonusPorAntiguedad anios 
+    | anios < 5 && anios >= 3 = 1.20
+    | anios < 10 = 1.30
+    | anios < 24 = 1.50
+    | anios > 24 = 2.2
+
+cantidadPorHoras :: Int -> Float
+cantidadPorHoras horasTrabajadas 
+    | horasTrabajadas <= 25 && horasTrabajadas > 10 =  2
+    | horasTrabajadas > 25 && horasTrabajadas < 35 =  3
+    | horasTrabajadas > 37 =  4
+    | otherwise = 1
+
+sueldoDocente :: String -> Int -> Int -> Float
+sueldoDocente puesto antiguedad horasTrabajadas = sueldoPorCargo puesto * cantidadPorHoras horasTrabajadas * bonusPorAntiguedad antiguedad
